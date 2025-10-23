ea3474b50adc8e86287d50d7ff4ace1c73c9c907
bhaigit <patuustake2@gmail.com>
2025-10-23 14:45:48 +0530

    Add new file

---
+++ app/(protected)/newfile.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const newfile = () => {
+  return (
+    <View>
+      <Text>newfile</Text>
+    </View>
+  )
+}
+
+export default newfile
