fb5a8fe1bb59f84d99b6e3058e2694213a7f5d82
bhaigit <patuustake2@gmail.com>
2025-10-24 01:36:07 +0530

    addElevenLabs

--- app/(protected)/index.tsx
+++ app/(protected)/index.tsx
@@ -1,21 +1,10 @@
 import SignOutButton from "@/components/clerk/SignOutButton";
 import { Gradient } from "@/components/gradient";
+import SessionScreen from "@/components/screens/SessionScreen";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <>
-        <Gradient position="top" isSpeaking={false}/>
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Home Screen</Text>
-      <SignOutButton />
-    </View>
-    </>
+    <SessionScreen />
   );
 }
--- components/screens/SessionScreen.tsx
+++ components/screens/SessionScreen.tsx
@@ -1,24 +1,54 @@
+import { useUser } from "@clerk/clerk-expo";
 import { useConversation } from "@elevenlabs/react-native";
-import { Text, View } from "react-native"
+import { Button, Text, View } from "react-native";
+import { red } from "react-native-reanimated/lib/typescript/Colors";
 
 export default function SessionScreen() {
 
-   const conversation = useConversation({
-  onConnect: () => console.log('Connected to conversation'),
-  onDisconnect: () => console.log('Disconnected from conversation'),
-  onMessage: (message) => console.log('Received message:', message),
-  onError: (error) => console.error('Conversation error:', error),
-  onModeChange: (mode) => console.log('Conversation mode changed:', mode),
-  onStatusChange: (prop) => console.log('Conversation status changed:', prop.status),
-  onCanSendFeedbackChange: (prop) =>
-    console.log('Can send feedback changed:', prop.canSendFeedback),
-  onUnhandledClientToolCall: (params) => console.log('Unhandled client tool call:', params),
-});
+const {user} = useUser(); 
 
+  const conversation = useConversation({
+    onConnect: () => console.log("Connected to conversation"),
+    onDisconnect: () => console.log("Disconnected from conversation"),
+    onMessage: (message) => console.log("Received message:", message),
+    onError: (error) => console.error("Conversation error:", error),
+    onModeChange: (mode) => console.log("Conversation mode changed:", mode),
+    onStatusChange: (prop) =>
+      console.log("Conversation status changed:", prop.status),
+    onCanSendFeedbackChange: (prop) =>
+      console.log("Can send feedback changed:", prop.canSendFeedback),
+    onUnhandledClientToolCall: (params) =>
+      console.log("Unhandled client tool call:", params),
+  });
 
-    return(
-        <View>
-            <Text>Session Screen</Text>
-        </View>
-    )
+  const startConversation = async () => {
+    try {
+      await conversation.startSession({
+        agentId: process.env.EXPO_PUBLIC_AGENT_ID,
+        dynamicVariables: {
+          user_name: user?.username ?? "Beto",
+          session_title: "test",
+          session_description: "test",
+        },
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const endCoversation = async () => {
+    try {
+      await conversation.endSession();
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return (
+    <View>
+      <Text>Session Screen</Text>
+      <Button title="Start conversation" onPress={startConversation} />
+      <Button title="End conversation" onPress={endCoversation} color={"red"} />
+    </View>
+  );
 }
